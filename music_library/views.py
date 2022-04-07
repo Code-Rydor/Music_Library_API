407a48ec30f82672133c32898262e54a43a65e02
Garrett <ga.landin89@gmail.com>
2022-04-07 17:08:34 -0500

    Completed delete functionality for music_library views file

--- music_library/views.py
+++ music_library/views.py
@@ -29,3 +29,6 @@ def music_detail(request, pk):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    elif request.method == 'DELETE':
+        music.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
